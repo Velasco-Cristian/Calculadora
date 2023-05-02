1f60c7b6db767df1b5ab5b27e75706b0b6fc2910
Velasco-Cristian <velascocristian.dev@gmail.com>
2023-05-02 16:19:13 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="vh-100 vw-100">
+      <h1 className="text-center p-3">Calculadora</h1>
+      <article>
+        <section>Texto</section>
+        <section>Calculadora</section>
+      </article>
+    </div>
+  );
+}
+
+export default App;
